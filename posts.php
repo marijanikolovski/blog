bfe2a45efa55395d64957014675eb703ece8c5e4
marijanikolovski <m.kuzeljevic@gmail.com>
2022-06-06 19:42:17 +0200

    Zadatak 3.

--- posts.php
+++ posts.php
@@ -1,7 +1,7 @@
 <div class="col-sm-8 blog-main">
 
 <div class="blog-post">
-    <h2 class="blog-post-title">Sample blog post</h2>
+    <h2 class="blog-post-title"><a href='#'>Sample blog post</a></h2>
     <p class="blog-post-meta">January 1, 2014 by <a href="#">Mark</a></p>
 
     <p>This blog post shows a few different types of content that's supported and styled with Bootstrap.
@@ -46,7 +46,7 @@
 </div><!-- /.blog-post -->
 
 <div class="blog-post">
-    <h2 class="blog-post-title">Another blog post</h2>
+    <h2 class="blog-post-title"><a href='#'>Another blog post</a></h2>
     <p class="blog-post-meta">December 23, 2013 by <a href="#">Jacob</a></p>
 
     <p>Cum sociis natoque penatibus et magnis <a href="#">dis parturient montes</a>, nascetur ridiculus
@@ -64,7 +64,7 @@
 </div><!-- /.blog-post -->
 
 <div class="blog-post">
-    <h2 class="blog-post-title">New feature</h2>
+    <h2 class="blog-post-title"><a href='#'>New feature</a></h2>
     <p class="blog-post-meta">December 14, 2013 by <a href="#">Chris</a></p>
 
     <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Aenean
